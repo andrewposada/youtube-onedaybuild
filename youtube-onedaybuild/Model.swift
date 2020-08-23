17daa0219b181dc02d517367a557a483ac8779c4
Andrew Posada <Andrew@Andrews-MacBook-Pro-4.local>
2020-08-23 15:07:55 -0700

    mdoified code before removing constants.swift

---
+++ youtube-onedaybuild/Model.swift
@@ -0,0 +1,9 @@
+//
+//  Model.swift
+//  youtube-onedaybuild
+//
+//  Created by Andrew Posada on 8/23/20.
+//  Copyright © 2020 Andrew Posada. All rights reserved.
+//
+
+import Foundation
---
+++ youtube-onedaybuild/Video.swift
@@ -0,0 +1,9 @@
+//
+//  Video.swift
+//  youtube-onedaybuild
+//
+//  Created by Andrew Posada on 8/23/20.
+//  Copyright © 2020 Andrew Posada. All rights reserved.
+//
+
+import Foundation
